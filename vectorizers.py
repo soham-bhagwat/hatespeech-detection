c6b61e2126ae43e48268f0c5419c96e26b5bddb3
Soham Bhagwat <thesohambhagwat@gmail.com>
2024-06-15 16:23:40 -0400

    vectorizers file added

---
+++ vectorizers.py
@@ -0,0 +1,92 @@
+import sys
+import re
+import nltk
+from nltk.stem.porter import *
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as VS
+import matplotlib.pyplot as plt
+from nltk.stem.porter import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as VS
+from textstat.textstat import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import classification_report
+from sklearn.svm import LinearSVC
+import numpy as np
+
+stopwords = nltk.corpus.stopwords.words('english')
+other_exclusions = ["#ff", "ff", "rt"]
+stopwords.extend(other_exclusions)
+stemmer = PorterStemmer()
+sentiment_analyzer = VS()
+
+def preprocess(text_string):
+    space_pattern = '\s+'
+    giant_url_regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|''[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    mention_regex = '@[\w\-]+'
+    parsed_text = re.sub(space_pattern,' ',text_string)
+    parsed_text = re.sub(giant_url_regex, 'URLHERE', parsed_text)
+    parsed_text = re.sub(mention_regex, 'MENTIONHERE', parsed_text)
+    return parsed_text
+
+
+def tokenize(tweet):
+    tweet = ' '.join(re.split("[^a-zA-Z]+", tweet.lower())).strip()
+    tokens = [stemmer.stem(t) for t in tweet.split()]
+    return tokens
+
+def basic_tokenize(tweet):
+    """Same as tokenize but without the stemming"""
+    tweet = " ".join(re.split("[^a-zA-Z.,!?]+", tweet.lower())).strip()
+    return tweet.split()
+
+
+sentiment_analyzer = VS()
+
+def count_twitter_objs(text_string):
+    space_pattern = "\s+"
+    giant_url_regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|''[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    mention_regex = '@[\w\-]+'
+    hashtag_regex = '#[\w\-]+'
+    parsed_text = re.sub(space_pattern, ' ', text_string)
+    parsed_text = re.sub(giant_url_regex, 'URLHERE', parsed_text)
+    parsed_text = re.sub(mention_regex, 'MENTIONHERE', parsed_text)
+    parsed_text = re.sub(hashtag_regex, 'HASHTAGHERE', parsed_text)
+    return(parsed_text.count('URLHERE'),parsed_text.count('MENTIONHERE'),parsed_text.count('HASHTAGHERE'))
+
+def other_features_(tweet):
+    """This function takes a string and returns a list of features.
+    These include Sentiment scores, Text and Readability scores,
+    as well as Twitter specific features"""
+    ##SENTIMENT
+    sentiment = sentiment_analyzer.polarity_scores(tweet)
+    
+    words = preprocess(tweet) #Get text only
+    
+    syllables = textstat.syllable_count(words) #count syllables in words
+    num_chars = sum(len(w) for w in words) #num chars in words
+    num_chars_total = len(tweet)
+    num_terms = len(tweet.split())
+    num_words = len(words.split())
+    avg_syl = round(float((syllables+0.001))/float(num_words+0.001),4)
+    num_unique_terms = len(set(words.split()))
+    
+    ###Modified FK grade, where avg words per sentence is just num words/1
+    FKRA = round(float(0.39 * float(num_words)/1.0) + float(11.8 * avg_syl) - 15.59,1)
+    ##Modified FRE score, where sentence fixed to 1
+    FRE = round(206.835 - 1.015*(float(num_words)/1.0) - (84.6*float(avg_syl)),2)
+    
+    twitter_objs = count_twitter_objs(tweet) #Count #, @, and http://
+    
+    features = [FKRA, FRE, syllables, num_chars, num_chars_total, num_terms, num_words,
+                num_unique_terms, sentiment['compound'],
+                twitter_objs[2],]
+    #features = pandas.DataFrame(features)
+    return features
+
+
+def get_feature_array_(tweets):
+    feats=[]
+    for t in tweets:
+        feats.append(other_features_(t))
+    return np.array(feats)
